1c3aef57a897683d63176bbdba573375e02650fe
GuilhermeSerafim <guilerstudies@gmail.com>
2025-05-07 15:41:14 -0300

    Deixando cor do foguete dinamica

--- src/app/components/binding/binding.component.ts
+++ src/app/components/binding/binding.component.ts
@@ -3,6 +3,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 
+
+
 @Component({
   selector: 'app-binding',
   imports: [FormsModule, MatInputModule, MatButtonModule],
@@ -10,11 +12,21 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './binding.component.scss',
 })
 export class BindingComponent {
+  maquinaEquipada = false;
+  corSelecionada = false;
+  cor?: string;
+  equiparMaquina() {
+    this.maquinaEquipada = true;
+  }
+
   private timerId!: number;
   count = 0;
 
   shipName = '';
   iniciarDecolagem(arg0: number) {
     this.count = arg0;
+  }
+  selecionarCor() {
+    this.corSelecionada = true;
   }
 }
